6d666364cd79c4247e08082d4a1bbc0a4182e693
Leslie Arkorful <lesliearkorful@gmail.com>
2023-08-16 21:52:24 +0000

    initial commit

---
+++ database/migrations/2023_08_16_210038_create_posts_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id('id');
+            $table->unsignedBigInteger('authorId');
+            $table->string('title');
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ database/migrations/2023_08_16_210303_create_comments_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id('id');
+            $table->unsignedBigInteger('postId');
+            $table->string('reply');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
